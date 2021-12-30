d56e9a559afea14a69eab71dbc02d19c40faf70e
Megghaanaa <91434485+Megghaanaa@users.noreply.github.com>
2021-12-30 21:22:21 +0530

    modified

--- tata.java
+++ tata.java
@@ -1,5 +1,5 @@
 public class tata {
     public static void main(String[] args) {
-        System.out.println("Up above the world so high!");
+        System.out.println("Up above the world so high Like a diamond in the sky!");
     }
 }
